a2416c6849a6ba55852c6628497fd92c6b00e73f
libjared <libjared@users.noreply.github.com>
2016-02-16 23:58:09 -0500

    disable convention-based routing

--- ShoppingDeals/App_Start/WebApiConfig.cs
+++ ShoppingDeals/App_Start/WebApiConfig.cs
@@ -12,12 +12,6 @@ namespace ShoppingDeals
         {
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
