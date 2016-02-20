858c7c03392e3c54a2900d3c289edaebda8c73ef
libjared <libjared@users.noreply.github.com>
2016-02-20 07:04:06 -0500

    add basic unit test for dealDB

---
+++ ShoppingDealsTests/DealsDBTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingDeals.Models;
+
+namespace ShoppingDeals.Controllers.Tests
+{
+    [TestClass]
+    public class DealsDBTests
+    {
+        private static DealsDB db;
+
+        [TestInitialize]
+        public static void InitDB(TestContext ctx)
+        {
+            db = new DealsDB();
+            db.Reinitialize();
+        }
+
+        [TestMethod]
+        public async Task TestGet()
+        {
+            IEnumerable<Deal> results = await db.GetDeals();
+        }
+    }
+}
