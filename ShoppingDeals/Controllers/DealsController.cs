47f4b862780f21995496b0778e63d821e2f2cecc
libjared <libjared@users.noreply.github.com>
2016-02-21 17:49:25 -0500

    clean usings

--- ShoppingDeals/Controllers/DealsController.cs
+++ ShoppingDeals/Controllers/DealsController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 using ShoppingDeals.Models;
 using System.Threading.Tasks;
