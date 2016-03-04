846e5133f965163d6bcd64041a39be002fc41af5
libjared <libjared@users.noreply.github.com>
2016-03-03 21:30:12 -0500

    post deal

--- ShoppingDeals/Controllers/DealsController.cs
+++ ShoppingDeals/Controllers/DealsController.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Web.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using ShoppingDeals.Models;
 
 // ReSharper disable UnusedMember.Global
 // ReSharper disable ClassNeverInstantiated.Global
@@ -20,10 +22,31 @@ namespace ShoppingDeals.Controllers
         }
 
         [Route("")]
+        [HttpGet]
         public async Task<HttpResponseMessage> GetDeals(string prod = null, string store = null, string zip = null)
         {
             var data = await db.GetDeals(prod, store, zip);
             return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> PostDeal([FromBody]JToken jsonbody)
+        {
+            if (!IsValidPostedDeal(jsonbody))
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            var postedDeal = jsonbody.ToObject<PostedDeal>();
+
+            await db.AddDeal(postedDeal.ToDeal());
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        private bool IsValidPostedDeal(JToken jsonbody)
+        {
+            return false;
         }
     }
 }
