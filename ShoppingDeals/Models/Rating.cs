5784e72f284dd49418bdc1d3e01aaf3945cfcb5b
libjared <libjared@users.noreply.github.com>
2016-03-04 08:25:10 -0500

    add Rating model

---
+++ ShoppingDeals/Models/Rating.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedMember.Global
+
+namespace ShoppingDeals.Models
+{
+    public class Rating
+    {
+        public ObjectId AboutDeal { get; set; }
+        public ObjectId AboutUser { get; set; }
+        public bool IsPositive { get; set; }
+
+        public ObjectId Id { get; set; }
+
+        public Rating(Deal aboutDeal, User aboutUser, bool isPositive)
+        {
+            AboutDeal = aboutDeal.Id;
+            AboutUser = aboutUser.Id;
+            IsPositive = isPositive;
+        }
+    }
+}
