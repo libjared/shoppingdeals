5d60042d2c5c126d0b09be1107bc0cc0c01d67b9
libjared <libjared@users.noreply.github.com>
2016-03-04 05:59:26 -0500

    admin endpoints to reformat the db

---
+++ ShoppingDeals/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace ShoppingDeals.Controllers
+{
+    [RoutePrefix("api/v1/admin")]
+    public class AdminController : ApiController
+    {
+        [Route("resetdb")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> ResetDb()
+        {
+            await StaticDealsDb.Db.Reinitialize();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
