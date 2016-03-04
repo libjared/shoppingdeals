5e06e65dc7597105fb214d4d539a890da36b5c0c
libjared <libjared@users.noreply.github.com>
2016-03-04 01:09:16 -0500

    fleshed out user model

--- ShoppingDeals/Models/User.cs
+++ ShoppingDeals/Models/User.cs
@@ -1,11 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ShoppingDeals.Models
+﻿namespace ShoppingDeals.Models
 {
     public class User
     {
+        public string Name { get; }
+        public string PasswordHash { get; }
+
+        public User(string name, string passwordHash)
+        {
+            Name = name;
+            PasswordHash = passwordHash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == this.GetType() && Equals((User)obj);
+        }
+
+        private bool Equals(User other)
+        {
+            return
+                string.Equals(Name, other.Name) &&
+                string.Equals(PasswordHash, other.PasswordHash);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name?.GetHashCode() ?? 0) * 397) ^ (PasswordHash?.GetHashCode() ?? 0);
+            }
+        }
     }
 }
