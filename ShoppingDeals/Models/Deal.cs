1ee8545c6ca873d00fbfbe287e7cdd33c427343e
libjared <libjared@users.noreply.github.com>
2016-03-03 21:26:05 -0500

    posteddeal

--- ShoppingDeals/Models/Deal.cs
+++ ShoppingDeals/Models/Deal.cs
@@ -11,9 +11,30 @@ namespace ShoppingDeals.Models
         public DateTime ExpirationDate { get; set; }
         public string Username { get; set; }
         public string ZipCode { get; set; }
-        public int Likes { get; private set; }
-        public int Dislikes { get; private set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
 
-        public ObjectId Id { get; private set; }
+        public ObjectId Id { get; set; }
+    }
+
+    public class PostedDeal
+    {
+        public string ProductName { get; set; }
+        public string StoreName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string ZipCode { get; set; }
+
+        public Deal ToDeal()
+        {
+            return new Deal
+            {
+                ProductName = ProductName,
+                StoreName = StoreName,
+                Price = Price,
+                ExpirationDate = ExpirationDate,
+                ZipCode = ZipCode
+            };
+        }
     }
 }
