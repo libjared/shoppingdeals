206bac441b4ee7d01ba5878820622e63c9ac01b3
libjared <libjared@users.noreply.github.com>
2016-03-04 06:11:46 -0500

    using cleanups

--- ShoppingDeals/Controllers/StaticDealsDb.cs
+++ ShoppingDeals/Controllers/StaticDealsDb.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ShoppingDeals.Controllers
+﻿namespace ShoppingDeals.Controllers
 {
     public static class StaticDealsDb
     {
